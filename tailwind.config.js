01a81798ea4cba62bd04ec39dea6c0d62ee0fbb8
Oluwashina <shinzbaba@gmail.com>
2023-05-09 17:08:32 +0100

    style:header layout design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Kalam400: ["Kalam"],
+        Kalam300: ["Kalam Light"],
+        Kalam700: ["Kalam Bold"],
+      },
+      backgroundImage: (theme) => ({
+        'main': "url('../dist/assets/Noise.svg')",
+        'paper': "url('../dist/assets/Paper.svg')",
+      }),
+    },
   },
   plugins: [],
 }
