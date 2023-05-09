221e094549686b46d685ccff8bdf58335f49a25a
Oluwashina <shinzbaba@gmail.com>
2023-05-09 22:49:48 +0100

    style:more sections added to portfolio layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
       backgroundImage: (theme) => ({
         'main': "url('../dist/assets/Noise.svg')",
         'paper': "url('../dist/assets/Paper.svg')",
+        'paper2': "url('../dist/assets/shadow_lower.svg')",
+        'paper3': "url('../dist/assets/contactPaper.svg')",
       }),
     },
   },
