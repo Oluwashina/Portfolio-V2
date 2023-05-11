2cbfd0dc2a65a6ae3b79d2f9698cad9d320d740a
Oluwashina <shinzbaba@gmail.com>
2023-05-11 13:42:52 +0100

    style:more sections of porfolio modification

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         'paper': "url('../dist/assets/Paper.svg')",
         'paper2': "url('../dist/assets/shadow_lower.svg')",
         'paper3': "url('../dist/assets/contactPaper.svg')",
+        'note': "url('../dist/assets/Note.svg')",
       }),
     },
   },
