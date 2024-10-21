427eeec158e58765e44c2993591e49e87a3e74b0
Oluwashina <shinzbaba@gmail.com>
2024-10-21 20:42:37 +0100

    fix: background image fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: (theme) => ({
+      backgroundImage: {
         'main': "url('../dist/assets/Noise.svg')",
         'paper': "url('../dist/assets/Paper.png')",
         'paper2': "url('../dist/assets/shadow_lower.svg')",
         'paper3': "url('../dist/assets/contactPaper.svg')",
         'note': "url('../dist/assets/Note.svg')",
 
-      }),
+      },
     },
   },
   plugins: [],
